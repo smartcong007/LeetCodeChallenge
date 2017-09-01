8186dee370bf6e698d4dbe86f466746cd9866c56
zc <zhengcong@souche.com>
2017-09-01 17:26:58 +0800

    add comments for 8 problems

---
+++ src/ReverseInteger.java
@@ -0,0 +1,20 @@
+/**
+ * Created by zhengcong on 2017/9/1.
+ */
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        long res = 0;
+        while (x!=0){
+
+            res=res*10+x%10;
+            x=x/10;
+        }
+        if(res>Integer.MAX_VALUE||res<Integer.MIN_VALUE){
+            return 0;
+        }else {
+            return (int) res;
+        }
+    }
+
+}
